e19e3ce5b04552aef893bcc35104940047c27ffc
David Pickup <pickup_david@hotmail.com>
2017-01-03 17:01:50 +0000

    Returns all information, but needs correcting and parsing

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('', function (req, res) {
-  res.send("Request header parser.");
+    var ip = req.ip;
+    var language = req.get('Accept-Language').split(',')[0];
+    var software = req.get('User-Agent');
+  res.send({
+      'ip':ip,
+      'language':language,
+      'software':software
+  });
 })
 
 app.listen(process.env.PORT || 5000, function () {
